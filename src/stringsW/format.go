8811c53255750350401da07a67f76fafb3b9cbf1
grewang <grewang@tencent.com>
2020-12-01 22:51:05 +0800

    bug fix

--- src/stringsW/format.go
+++ src/stringsW/format.go
@@ -5,30 +5,34 @@ import (
 	"strings"
 )
 
-func Wrap(s string, width int, indent int) (string, error) {
+func Wrap(s string, width, indent int, dilimiter string) (string, error) {
 	words := SplitNoEmpty(s, " ")
 	lines := make([]string, 0, 8)
 	curLine := make([]string, 0, 8)
-	cursor := indent
-	indentString := strings.Repeat(" ", indent)
+	cursor := 0
 
 	for _, word := range words {
-		word = indentString + strings.TrimSpace(word)
+		word = strings.TrimSpace(word)
 		if cursor+len(word) > width {
-			lines = append(lines, strings.Join(curLine, "\n"))
+			lines = append(lines, strings.Join(curLine, dilimiter))
 			curLine = curLine[:0]
 			if len(word) > width {
 				return s, fmt.Errorf("width (%d) is too small", width)
 			}
-			cursor = len(word)
-			curLine = append(curLine, word)
+			cursor = len(word) + indent
+			curLine = append(curLine, word+dilimiter)
 		} else {
-			cursor += len(word)
-			curLine = append(curLine, word)
+			cursor += len(word) + len(dilimiter)
+			curLine = append(curLine, word+dilimiter)
 		}
 	}
 	if len(curLine) > 0 {
-		lines = append(lines, strings.Join(curLine, "\n"))
+		lines = append(lines, strings.Join(curLine, dilimiter))
 	}
-	return strings.Join(lines, "\n"), nil
+	res := ""
+	for _, line := range lines {
+		// fmt.Printf("|%s|\n", line)
+		res += strings.Repeat(" ", indent) + line + "\n"
+	}
+	return res, nil
 }
