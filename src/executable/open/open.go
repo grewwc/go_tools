4f1b99a581dce80b8ed1a57eff3aa518a477ddc1
wwc129 <mamawwc@gmail.com>
2021-10-30 15:39:00 +0800

    bug fix

--- src/executable/open/open.go
+++ src/executable/open/open.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"syscall"
 
 	"github.com/grewwc/go_tools/src/stringsW"
 	"github.com/grewwc/go_tools/src/utilsW"
@@ -23,15 +25,17 @@ func main() {
 	}
 	switch utilsW.GetPlatform() {
 	case utilsW.WINDOWS:
-		cmdStr = fmt.Sprintf("cmd /C start %q", dir)
+		cmdStr = fmt.Sprintf(`cmd /C start "" %q`, dir)
 	case utilsW.MAC:
 		cmdStr = fmt.Sprintf("/usr/bin/open %q", dir)
 	case utilsW.LINUX:
 		cmdStr = fmt.Sprintf("xdg-open %q", dir)
 	}
 	cmdSlice := stringsW.SplitNoEmpty(cmdStr, " ")
-	cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
-
+	cmd := exec.Command(cmdSlice[0])
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	// fmt.Println()
+	cmd.SysProcAttr.CmdLine = strings.Join(cmdSlice[1:], " ")
 	if err := cmd.Run(); err != nil {
 		log.Fatalln(err)
 	}
