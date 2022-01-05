df89840d2cdcad63b4957d836f87528f4f106982
之达 <wangwenchao.wwc@alibaba-inc.com>
2022-01-05 23:11:43 +0800

    bug fix

--- src/executable/memo/mm.go
+++ src/executable/memo/mm.go
@@ -954,8 +954,8 @@ func main() {
 					filename := content[:idx]
 					title := content[idx+1:]
 					if !utilsW.IsExist(filename) ||
-						(utilsW.IsExist(filename) && _helpers.PromptYesOrNo((fmt.Sprintf("%q already exists, do you want ot overwirte it? (y/n): ", filename)))) ||
-						(utilsW.IsExist(filename) && parsed.ContainsFlagStrict("force")) {
+						(utilsW.IsExist(filename) && parsed.ContainsFlagStrict("force")) ||
+						(utilsW.IsExist(filename) && _helpers.PromptYesOrNo((fmt.Sprintf("%q already exists, do you want ot overwirte it? (y/n): ", filename)))) {
 						if err := ioutil.WriteFile(filename, []byte(title), 0666); err != nil {
 							panic(err)
 						}
