4b8d3ce26dd3a3cf7003817fbc3157e2e58518a7
之达 <wangwenchao.wwc@alibaba-inc.com>
2024-12-24 20:54:54 +0800

    lru cache

---
+++ src/containerW/lru_cache.go
@@ -0,0 +1,91 @@
+package containerW
+
+type node struct {
+	key   interface{}
+	value interface{}
+	next  *node
+	prev  *node
+}
+
+type LruCache struct {
+	_map  map[interface{}]*node
+	head  *node
+	tail  *node
+	cap   uint
+	count uint
+}
+
+func NewLruCache(cap uint) *LruCache {
+	ret := LruCache{cap: cap, _map: make(map[interface{}]*node)}
+	ret.head = &node{}
+	return &ret
+}
+
+func (cache *LruCache) Get(key interface{}) interface{} {
+	n, ok := cache._map[key]
+	if !ok {
+		return nil
+	}
+	cache.moveToHead(n)
+	return n.value
+}
+
+func (cache *LruCache) Put(key interface{}, value interface{}) {
+	n, ok := cache._map[key]
+	if !ok {
+		newNode := &node{key: key, value: value}
+		cache._map[key] = newNode
+		cache.moveToHead(newNode)
+		cache.count++
+		if cache.tail == nil {
+			cache.tail = newNode
+		}
+		if cache.count > cache.cap {
+			cache.removeTail()
+		}
+
+	} else {
+		n.value = value
+		cache.moveToHead(n)
+	}
+}
+
+func (cache *LruCache) moveToHead(n *node) {
+	currHead := cache.head.next
+	if n == currHead {
+		return
+	}
+	next := n.next
+	prev := n.prev
+	if next != nil {
+		next.prev = prev
+	}
+	if prev != nil {
+		prev.next = next
+	}
+	cache.head.next = n
+	n.prev = cache.head
+	n.next = currHead
+	if currHead != nil {
+		currHead.prev = n
+	}
+	if n == cache.tail {
+		cache.tail = prev
+	}
+}
+
+func (cache *LruCache) removeTail() {
+	if cache.tail == nil {
+		return
+	}
+	prev := cache.tail.prev
+
+	if _, ok := cache._map[cache.tail.key]; ok {
+		delete(cache._map, cache.tail.key)
+		cache.count--
+	}
+	if prev != nil {
+		prev.next = nil
+	}
+	cache.tail = prev
+}
---
+++ test/container/cache_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/grewwc/go_tools/src/containerW"
+)
+
+func TestLruCache(t *testing.T) {
+	c := containerW.NewLruCache(2)
+	c.Put("k1", 1)
+	c.Put("k2", 2)
+	if c.Get("k1").(int) != 1 {
+		t.Fatal("error")
+	}
+	c.Put("k3", 3)
+	if c.Get("k2") != nil {
+		t.Fatal("k2 should be nil")
+	}
+	c.Get("k2")
+	c.Get("k3")
+	if c.Get("k1").(int) != 1 {
+		t.Fatal("k1 should be 1")
+	}
+	c.Get("k4")
+	c.Get("k3")
+	c.Put("k4", 4)
+	if c.Get("k4") != 4 {
+		t.Fatal("k4 should be 4")
+	}
+	if c.Get("k1") != nil {
+		t.Fatal("k1 should be nil")
+	}
+}
