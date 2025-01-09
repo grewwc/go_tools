805367b2356fc7552b5d6aa3bd0e40d221f4d584
之达 <wangwenchao.wwc@alibaba-inc.com>
2025-01-09 12:38:40 +0800

    update: ns

--- go.mod
+++ go.mod
@@ -7,15 +7,20 @@ require (
 	github.com/fatih/color v1.13.0
 	github.com/google/uuid v1.3.0
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
+	github.com/shirou/gopsutil v2.21.11+incompatible
 	go.mongodb.org/mongo-driver v1.9.1
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
-	golang.org/x/sys v0.24.0
+	golang.org/x/sys v0.28.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.18.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 )
 
 require (
@@ -26,7 +31,6 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
@@ -36,5 +40,4 @@ require (
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- src/executable/ns/ns.go
+++ src/executable/ns/ns.go
@@ -1,28 +1,84 @@
+//go:build !windows
+// +build !windows
+
 package main
 
 import (
-	"log"
-	"os"
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/grewwc/go_tools/src/utilsW"
+	"github.com/shirou/gopsutil/net"
 )
 
-func main() {
-	conf := utilsW.GetAllConfig()
-	if conf == nil {
-		log.Fatalln(conf)
-		return
+func selectEn0(stats []net.IOCountersStat, name string) *net.IOCountersStat {
+	for _, stat := range stats {
+		if stat.Name == name {
+			return &stat
+		}
 	}
-	cmd := conf.GetOrDefault("utils.ns.cmd", nil)
-	if cmd == nil {
-		log.Fatalf("need to set utils.ns.cmd in ~/.configW")
-		return
-	}
-	cmdStr := cmd.(string)
-	cmdStr += " " + strings.Join(os.Args[1:], " ")
+	return nil
+}
 
-	if err := utilsW.RunCmd(cmdStr); err != nil {
-		log.Fatalln(err)
+func changeFormat(speed float64) string {
+	unit := "k/s"
+	if speed > 1024 {
+		speed /= 1024
+		unit = "m/s"
+	}
+	return fmt.Sprintf("%.2f %s", speed, unit)
+}
+
+func detectInterface() string {
+	if utilsW.GetPlatform() == utilsW.LINUX {
+		return "eth0"
+	}
+	return "en0"
+}
+
+func diff(prev, now *info) (float64, float64) {
+	sentDiff := float64((now.sent - prev.sent))
+	downloadDiff := float64(now.recv - prev.recv)
+
+	return sentDiff, downloadDiff
+}
+
+type info struct {
+	sent, recv float64
+	tp         int64
+}
+
+func showSpeed(ch chan<- *info) {
+	interval := 1
+	stats, err := net.IOCounters(true)
+	if err != nil {
+		panic(err)
+	}
+	interface_ := detectInterface()
+	for {
+		stats, _ = net.IOCounters(true)
+		currStat := selectEn0(stats, interface_)
+		ch <- &info{sent: float64(currStat.BytesSent), recv: float64(currStat.BytesRecv), tp: time.Now().UnixMilli()}
+		time.Sleep(time.Second * time.Duration(interval))
+	}
+}
+
+func main() {
+	ch := make(chan *info)
+	go showSpeed(ch)
+	var prev *info
+	var sent, download float64
+	for curr := range ch {
+		if prev == nil {
+			prev = curr
+			fmt.Printf("    Ul: %s    Dl: %s%s\r", changeFormat(sent), changeFormat(float64(download)), strings.Repeat(" ", 20))
+			continue
+		}
+		sent, download = diff(prev, curr)
+		dt := float64(curr.tp-prev.tp) / 1e3
+		sent, download = sent/1024/float64(dt), download/1024/float64(dt)
+		fmt.Printf("    Ul: %s    Dl: %s%s\r", changeFormat(sent), changeFormat(download), strings.Repeat(" ", 20))
+		prev = curr
 	}
 }
