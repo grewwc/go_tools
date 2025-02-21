77193715b9686d6c5fa20b860e3f4168afdcae5e
之达 <wangwenchao.wwc@alibaba-inc.com>
2025-02-21 17:29:40 +0800

    add:stringsW

---
+++ src/stringsW/check.go
@@ -0,0 +1,36 @@
+package stringsW
+
+import "unicode"
+
+func IsBlank(str string) bool {
+	if str == "" {
+		return true
+	}
+	for _, ch := range str {
+		if !unicode.IsSpace(ch) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsAllBlank(strs ...string) bool {
+	if len(strs) == 0 {
+		return false
+	}
+	for _, str := range strs {
+		if !IsBlank(str) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsAnyBlank(strs ...string) bool {
+	for _, str := range strs {
+		if IsBlank(str) {
+			return true
+		}
+	}
+	return false
+}
