9a59e8b4bc755b57ef1d4b843819480b68a7f095
之达 <wangwenchao.wwc@alibaba-inc.com>
2025-03-25 17:52:39 +0800

    update:string mul

--- test/strings/math_test.go
+++ test/strings/math_test.go
@@ -1,10 +1,13 @@
 package test
 
 import (
+	"fmt"
 	"math/big"
+	"math/rand"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/grewwc/go_tools/src/numW"
 	"github.com/grewwc/go_tools/src/strW"
@@ -44,10 +47,38 @@ func TestMul(t *testing.T) {
 		expect := ba.Mul(ba, bb)
 		res := strW.Mul(strconv.Itoa(a), strconv.Itoa(b))
 		if expect.String() != res {
-			t.Errorf("Mul(%d, %d) = %s, want |%s|", a, b, expect.String(), res)
+			t.Errorf("Mul(%d, %d) = %s, want |%s|", a, b, res, expect.String())
 		}
 	}
 
+}
+
+func genRandomNumber(n int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	// 定义最终结果字符串
+	var result string
+
+	// 循环生成随机整数并拼接到结果字符串中
+	for len(result) < n {
+		// 生成一个随机整数
+		randomInt := rand.Intn(10) // 生成 0 到 9 之间的随机整数
+		// 将整数转换为字符串并追加到结果字符串中
+		result += fmt.Sprintf("%d", randomInt)
+	}
+
+	return result
+}
+
+func BenchmarkMul(b *testing.B) {
+	n := 5000
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		s1 := genRandomNumber(n)
+		s2 := genRandomNumber(n)
+		b.StartTimer()
+		strW.Mul(s1, s2)
+	}
 }
 
 func TestDiv(t *testing.T) {
