49b015be6809d91ed2aaf4bc3161f20bb3868de5
之达 <wangwenchao.wwc@alibaba-inc.com>
2025-04-14 23:52:27 +0800

    update:bloom_filter(delete?)

---
+++ src/cw/bloom_filter.go
@@ -0,0 +1,86 @@
+package cw
+
+import (
+	"fmt"
+	"sync/atomic"
+
+	"github.com/grewwc/go_tools/src/algow"
+	"github.com/grewwc/go_tools/src/typew"
+)
+
+type BloomFilter[T any] struct {
+	data      []uint64
+	hasher    typew.HashFunc[string]
+	capacity  int
+	hashTimes int
+}
+
+func NewBloomFilter[T any](capacity int) *BloomFilter[T] {
+	hasher := typew.CreateDefaultHash[string]()
+	data := make([]uint64, capacity)
+	return &BloomFilter[T]{
+		hasher:    hasher,
+		data:      data,
+		capacity:  capacity,
+		hashTimes: 3,
+	}
+}
+
+func (f *BloomFilter[T]) MayExist(item T) bool {
+	hashVal := f.hash(item)
+	cnt := algow.Max(f.numDigit(hashVal), f.hashTimes)
+	for i := 0; i < cnt; i++ {
+		old := atomic.LoadUint64(&f.data[hashVal])
+		if old == 0 {
+			return false
+		}
+		hashVal = f.hasher(fmt.Sprintf("%v%d", item, hashVal)) % f.capacity
+	}
+	return true
+}
+
+func (f *BloomFilter[T]) Add(item T) bool {
+	hashVal := f.hash(item)
+	swapped := true
+	cnt := algow.Max(f.numDigit(hashVal), f.hashTimes)
+	for i := 0; i < cnt; i++ {
+		old := atomic.LoadUint64(&f.data[hashVal])
+		swapped = swapped && atomic.CompareAndSwapUint64(&f.data[hashVal], old, old+1)
+		hashVal = f.hasher(fmt.Sprintf("%v%d", item, hashVal)) % f.capacity
+	}
+	return swapped
+}
+
+func (f *BloomFilter[T]) Delete(item T) bool {
+	hashVal := f.hash(item)
+	swapped := true
+	cnt := algow.Max(f.numDigit(hashVal), f.hashTimes)
+	for i := 0; i < cnt; i++ {
+		old := atomic.LoadUint64(&f.data[hashVal])
+		if old == 0 {
+			return false
+		}
+		swapped = swapped && atomic.CompareAndSwapUint64(&f.data[hashVal], old, old-1)
+		hashVal = f.hasher(fmt.Sprintf("%v%d", item, hashVal)) % f.capacity
+	}
+	return swapped
+}
+
+func (f *BloomFilter[T]) spread(h int) int {
+	uh := uint(h)
+	return int((uh ^ (uh >> 16)) & 0x7fffffff)
+}
+
+func (f *BloomFilter[T]) hash(item T) int {
+	s := fmt.Sprintf("%v", item)
+	return f.spread(f.hasher(s)) % f.capacity
+}
+
+func (f *BloomFilter[T]) numDigit(val int) int {
+	ret := 0
+	for val > 0 {
+		ret++
+		val = val >> 1
+	}
+	return ret
+}
---
+++ test/container/bloom_filter_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/grewwc/go_tools/src/cw"
+)
+
+func TestBloomFilter(t *testing.T) {
+	N := 100
+	f := cw.NewBloomFilter[int](N * 100)
+	var wg sync.WaitGroup
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			f.Add(i)
+		}(i)
+	}
+
+	for i := 0; i < N/2; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			f.Delete(i)
+		}(i)
+	}
+
+	for i := N / 2; i < N; i++ {
+		if !f.MayExist(i) {
+			t.Error("wrong")
+		}
+	}
+	cnt := 0
+	for i := 0; i < 10*N; i++ {
+		if f.MayExist(i) {
+			cnt++
+		}
+	}
+	t.Logf("stats: %d/%d", cnt, N)
+}
