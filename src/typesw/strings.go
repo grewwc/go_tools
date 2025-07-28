ac45f2a97ee76d0d45bbf2cfbcf896cc51f2f38d
之达 <wangwenchao.wwc@alibaba-inc.com>
2025-07-29 02:07:15 +0800

    update:str2bytes

--- src/typesw/strings.go
+++ src/typesw/strings.go
@@ -1,28 +1,12 @@
 package typesw
 
-import (
-	"reflect"
-	"unsafe"
-)
-
 // StrToBytes converts a string to []byte without copying.
 func StrToBytes(s string) []byte {
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	bh := reflect.SliceHeader{
-		Data: sh.Data,
-		Len:  sh.Len,
-		Cap:  sh.Len,
-	}
-	return *(*[]byte)(unsafe.Pointer(&bh))
+	return []byte(s)
 }
 
 // BytesToStr converts []byte to string without copying.
 // This is safe because strings are immutable in Go.
 func BytesToStr(b []byte) string {
-	sh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	bh := reflect.StringHeader{
-		Data: sh.Data,
-		Len:  sh.Len,
-	}
-	return *(*string)(unsafe.Pointer(&bh))
+	return string(b)
 }
