b980a886c8e79ccf10ae50328b0e08d335541589
grewwc <mamawwc@gmail.com>
2025-08-14 10:34:41 +0800

    update:lru_cache

--- src/cw/lru_cache.go
+++ src/cw/lru_cache.go
@@ -8,7 +8,7 @@ type node struct {
 }
 
 type LruCache struct {
-	_map  map[interface{}]*node
+	m     map[interface{}]*node
 	head  *node
 	tail  *node
 	cap   uint
@@ -16,76 +16,68 @@ type LruCache struct {
 }
 
 func NewLruCache(cap uint) *LruCache {
-	ret := LruCache{cap: cap, _map: make(map[interface{}]*node)}
+	ret := LruCache{cap: cap, m: make(map[interface{}]*node)}
 	ret.head = &node{}
+	ret.tail = &node{}
+	ret.head.next = ret.tail
+	ret.tail.prev = ret.head
 	return &ret
 }
 
 func (cache *LruCache) Get(key interface{}) interface{} {
-	n, ok := cache._map[key]
+	n, ok := cache.m[key]
 	if !ok {
 		return nil
 	}
-	cache.moveToHead(n)
+	cache.moveToFront(n)
 	return n.value
 }
 
 func (cache *LruCache) Put(key interface{}, value interface{}) {
-	n, ok := cache._map[key]
+	n, ok := cache.m[key]
 	if !ok {
 		newNode := &node{key: key, value: value}
-		cache._map[key] = newNode
-		cache.moveToHead(newNode)
+		cache.m[key] = newNode
+		cache.insertToFront(newNode)
 		cache.count++
-		if cache.tail == nil {
-			cache.tail = newNode
-		}
 		if cache.count > cache.cap {
 			cache.removeTail()
 		}
 
 	} else {
 		n.value = value
-		cache.moveToHead(n)
+		cache.moveToFront(n)
 	}
 }
 
-func (cache *LruCache) moveToHead(n *node) {
-	currHead := cache.head.next
-	if n == currHead {
-		return
-	}
-	next := n.next
-	prev := n.prev
-	if next != nil {
-		next.prev = prev
-	}
-	if prev != nil {
-		prev.next = next
-	}
+func (cache *LruCache) insertToFront(n *node) {
+	front := cache.head.next
+
 	cache.head.next = n
 	n.prev = cache.head
-	n.next = currHead
-	if currHead != nil {
-		currHead.prev = n
-	}
-	if n == cache.tail {
-		cache.tail = prev
-	}
+
+	n.next = front
+	front.prev = n
+}
+
+func (cache *LruCache) moveToFront(n *node) {
+	next := n.next
+	prev := n.prev
+	next.prev = prev
+	prev.next = next
+
+	front := cache.head.next
+	cache.head.next = n
+	n.prev = cache.head
+
+	n.next = front
+	front.prev = n
 }
 
 func (cache *LruCache) removeTail() {
-	if cache.tail == nil {
-		return
-	}
-	prev := cache.tail.prev
-
-	if _, ok := cache._map[cache.tail.key]; ok {
-		delete(cache._map, cache.tail.key)
-		cache.count--
-	}
-	if prev != nil {
-		prev.next = nil
-	}
-	cache.tail = prev
+	tail := cache.tail.prev
+	tail.prev.next = tail
+	cache.tail.prev = tail.prev
+	delete(cache.m, tail.key)
+	tail = nil
 }
